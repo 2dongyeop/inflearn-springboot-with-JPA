86cc7edfa1592dcbd60ca9f8dfcc2351118df222
leedongyeop <ldy_1204@naver.com>
2023-01-19 21:23:26 +0900

    feat: 주문 검색 기능 개발

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -28,6 +28,12 @@ public class OrderRepository {
 
     /**
      * 검색 기능
+     * Querydsl를 이용 → 권장 방법!!
+     */
+//    public List<Order> findAll(OrderSearch orderSearch) {}
+    
+
+    /**
      * 동적 쿼리가 아닌 직접 쿼리 작성하는 방식 : 권장x
      */
     public List<Order> findAllByString(OrderSearch orderSearch) {
