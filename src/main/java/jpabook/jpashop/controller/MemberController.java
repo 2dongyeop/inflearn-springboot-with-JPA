4f180a8229ab4fa8968d5647cc1f148771f55a14
leedongyeop <ldy_1204@naver.com>
2023-01-20 16:59:35 +0900

    feat: 회원 등록 로직 개발

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.controller;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm"; //이 html로 이동
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) { //@Valid -> @NotEmpty와 같은 기능을 검사
+
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member); //저장
+        return "redirect:/"; //저장이 끝나면 홈으로 이동
+    }
+}
