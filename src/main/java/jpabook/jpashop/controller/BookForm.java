4445ccc0660db049f8fe97e267c3f5ad8340be37
leedongyeop <ldy_1204@naver.com>
2023-01-21 17:29:24 +0900

    feat: 상품 등록 로직

---
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class BookForm {
+
+    private Long id;
+
+    //Item 공통 속성
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    //Book만 해당하는 속성
+    private String author;
+    private String isbn;
+}
