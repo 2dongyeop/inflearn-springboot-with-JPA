c5e4fa91f54bfb7bc7f2f2a0aa35d7f7e9f1e071
leedongyeop <ldy_1204@naver.com>
2023-03-14 15:02:44 +0900

    feat: 회원 조회 API ver2 개발

---
+++ src/main/java/jpabook/jpashop/InitDb.java
@@ -0,0 +1,9 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class InitDb {
+
+
+}
--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,7 +33,35 @@ public class MemberApiController {
     public List<Member> membersV1() {
         return memberService.findMembers();
     }
-    
+
+
+    /**
+     * 회원 조회 v2 : 응답 값으로 엔티티가 아닌 별도의 DTO를 반환한다.
+     */
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+
+        List<Member> findMembers = memberService.findMembers();
+
+        List<MemberDto> collect = findMembers.stream()
+                .map(member -> new MemberDto(member.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
 
     /**
      * 회원 등록 v1 : 요청 값으로 Member 엔티티를 직접 받는다.
