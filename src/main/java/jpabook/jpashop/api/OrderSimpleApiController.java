be5468698b1b6c960ecf29a7a9f8da8744da28e5
leedongyeop <ldy_1204@naver.com>
2023-03-17 20:09:55 +0900

    feat: 간단한 주문 조회 v3 : 엔티티를 DTO로 변환 - 페치 조인 최적화

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -27,7 +27,7 @@ public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
 
-    /**
+    /*
      * 주문 조회 V1 : 엔티티 직접 노출
      * - Hibernate5Module 모듈을 등록해야 함 -> LAZY = null 처리
      * - 양방향 관계 문제 발생 -> @JsonIgnore
@@ -43,7 +43,9 @@ public class OrderSimpleApiController {
         return all;
     }
 
-
+    /*
+     * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X) * - 단점: 지연로딩으로 쿼리 N번 호출
+     */
     @GetMapping("/api/v2/simple-orders")
     public Result ordersV2() {
         //쿼리 1번 발생 -> 총 결과가 2개나옴 (회원, 배송)
@@ -54,11 +56,27 @@ public class OrderSimpleApiController {
                 .map(order -> new SimpleOrderDto(order))
                 .collect(Collectors.toList());
 
-        //이게 N + 1문제 (== 1 + N) 
+        //이게 N + 1문제 (== 1 + N)
 
-        //TODO : 반환 List를 여따 넣기
         return new Result(orderDtoList);
     }
+
+
+    /*
+     * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
+     * - fetch join으로 쿼리 1번 호출
+     * 참고: fetch join에 대한 자세한 내용은 JPA 기본편 참고(정말 중요함) */
+    @GetMapping("/api/v3/simple-orders")
+    public Result ordersV3() {
+        List<Order> orderList = orderRepository.findAllByMemberDelivery();
+
+        List<SimpleOrderDto> orderDtoList = orderList.stream()
+                .map(order -> new SimpleOrderDto(order))
+                .collect(Collectors.toList());
+
+        return new Result(orderDtoList);
+    }
+
 
     @Data
     @AllArgsConstructor
